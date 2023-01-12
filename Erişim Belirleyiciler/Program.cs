8a394f7b8e9fb77f35dfdfef91bb9c81200098cd
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-12 13:52:07 +0300

    Proje dosyası ekle.

---
+++ Erişim Belirleyiciler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Erişim_Belirleyiciler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kisiler kisi=new kisiler();
+            kisi.word = "Serhan KORAL";
+            Console.WriteLine(kisi.word);
+            kisi.writing("Serhna","Koral",18);
+        }
+    }
+}
---
+++ Erişim Belirleyiciler/kisiler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Erişim_Belirleyiciler
+{
+    internal class kisiler
+    {
+        private string name;
+        private string surname;
+        private int age;
+        public string word;
+
+        public void writing(string ad, string soyad, int yas)
+        {
+            word = ad + " " + soyad +" "+yas + " yaşındadır";
+            Console.WriteLine(word);
+        }
+    }
+}
